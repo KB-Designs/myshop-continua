03c76b562881e107a09e3876e4853a8e4c5fbe13
Brian <kimanibrian030@gmail.com>
2025-08-20 22:54:57 +0300

    .

--- shop/models.py
+++ shop/models.py
@@ -15,8 +15,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    def get_absolute_urls(self):
-        return reverse('product_list_by_category',
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
                        args=[self.slug])
 
 class Product(models.Model):
